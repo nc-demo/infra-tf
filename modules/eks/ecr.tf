156647c24715b126c1a98934af18d2a18992f922
Peter Butkovic <butkovic@gmail.com>
2021-08-10 21:03:32 +0200

    create k8s secret in eks to access ecr

--- modules/eks/ecr.tf
+++ modules/eks/ecr.tf
@@ -3,48 +3,6 @@ resource "aws_ecr_repository" "repo" {
   image_tag_mutability = "IMMUTABLE"
 }
 
-//# security/policy
-//resource "aws_iam_policy" "read" {
-//  name        = format("%s-ecr-read", "nc-demo")
-//  description = format("Allow to read images from the ECR")
-//  path        = "/"
-//  policy = jsonencode({
-//    Statement = [{
-//      Action = [
-//        "ecr:BatchCheckLayerAvailability",
-//        "ecr:BatchGetImage",
-//        "ecr:DescribeImages",
-//        "ecr:DescribeRepositories",
-//        "ecr:GetAuthorizationToken",
-//        "ecr:GetDownloadUrlForLayer",
-//        "ecr:ListImages"
-//      ]
-//      Effect   = "Allow"
-//      Resource = [aws_ecr_repository.repo.arn]
-//    }]
-//    Version = "2012-10-17"
-//  })
-//}
-//
-//resource "aws_iam_policy" "write" {
-//  name        = format("%s-ecr-write", "nc-demo")
-//  description = format("Allow to push and write images to the ECR")
-//  path        = "/"
-//  policy = jsonencode({
-//    Statement = [{
-//      Action = [
-//        "ecr:PutImage",
-//        "ecr:UploadLayerPart",
-//        "ecr:InitiateLayerUpload",
-//        "ecr:CompleteLayerUpload",
-//      ]
-//      Effect   = "Allow"
-//      Resource = [aws_ecr_repository.repo.arn]
-//    }]
-//    Version = "2012-10-17"
-//  })
-//}
-
 resource "aws_ecr_repository_policy" "nc-demo-policy" {
   repository = aws_ecr_repository.repo.name
   policy     = <<EOF
@@ -73,4 +31,36 @@ resource "aws_ecr_repository_policy" "nc-demo-policy" {
     ]
   }
   EOF
+}
+
+data "aws_ecr_authorization_token" "token" {
+}
+
+locals {
+  service_name = "users"
+  login_server = aws_ecr_repository.repo.repository_url
+  username = data.aws_ecr_authorization_token.token.user_name
+  password = data.aws_ecr_authorization_token.token.password
+}
+
+locals {
+  docker-credentials = {
+    auths = {
+      "${local.login_server}" = {
+        auth = base64encode("${local.username}:${local.password}")
+      }
+    }
+  }
+}
+
+resource "kubernetes_secret" "docker_credentials" {
+  metadata {
+    name = "nc-demo"
+  }
+
+  data = {
+    ".dockerconfigjson" = jsonencode(local.docker-credentials)
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
 }
