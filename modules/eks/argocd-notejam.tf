b2cd3b1bcf0b815c933f8d935c61d0323179f48a
Peter Butkovic <butkovic@gmail.com>
2021-08-12 21:15:39 +0200

    fixed argo syntax

--- modules/eks/argocd-notejam.tf
+++ modules/eks/argocd-notejam.tf
@@ -15,20 +15,17 @@ spec:
     path: notejam/
     repoURL: https://github.com/nc-demo/k8s-env-poc
     targetRevision: main
-  sync:
-    comparedTo:
-      destination:
-        namespace: default
-        server: https://kubernetes.default.svc
-      source:
-        path: notejam/
-        repoURL: https://github.com/nc-demo/k8s-env-poc
-        targetRevision: main
   syncPolicy:
     automated:
       prune: true
       selfHeal: true
     syncOptions:
     - CreateNamespace=true
+    retry:
+      limit: 5 # number of failed sync attempt retries; unlimited number of attempts if less than 0
+      backoff:
+        duration: 5s # the amount to back off. Default unit is seconds, but could also be a duration (e.g. "2m", "1h")
+        factor: 2 # a factor to multiply the base duration after each failed retry
+        maxDuration: 3m # the maximum amount of time allowed for the backoff strategy
 YAML
 }
